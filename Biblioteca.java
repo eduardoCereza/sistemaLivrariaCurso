5f4369c3307baa5f11fda0ed02daab86eb932b58
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-10-28 21:26:01 -0300

    Fazendo estrutura de início e fim

--- Biblioteca.java
+++ Biblioteca.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Biblioteca {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Deseja ver a lista de livros disponíveis?");
+
+        String resposta = scanner.nextLine();
+
+        switch (resposta){
+            case "sim":
+                break;
+            case "não":
+                System.out.println("Obrigado! Sistema está finalizando... Até mais!");
+                break;
+        }
+
+    }
 }
